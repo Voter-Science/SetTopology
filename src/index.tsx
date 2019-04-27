e9dcd970f688a81f9a25ba558bca424b8c243349
Voter-Science <Info@Voter-Science.com>
2019-04-27 16:34:57 -0700

    addung trc

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,42 @@ import * as ReactDOM from "react-dom";
 
 import { Hello } from "./components/Hello";
 
+// declare var _sheetRef : any; // set by PluginMain, after page load.
+
+export interface IMajorProps { }
+export interface IMajorState {
+    AuthToken: string;
+    SheetId: string;
+}
+
+export class Major extends React.Component<{}, IMajorState> {
+    public constructor(props: any) {
+        super(props);
+        // this.state = { SheetId: undefined };
+
+        var x: any = window;
+        x.mainMajor = this;
+    }
+    render() {
+        return <h1>Major! {this.state.SheetId}</h1>;
+    }
+
+    // Timer to pick up _sheetRef? 
+
+    public setSheetRef(sheetRef: any): void {
+        // Make async network call...
+        this.setState({ 
+            SheetId: sheetRef.SheetId,
+            AuthToken : sheetRef.AuthToken
+        });
+    }
+}
+
+
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
+    <p>
+        <Major></Major>
+        <Hello compiler="TypeScript" framework="React" />
+    </p>,
     document.getElementById("example")
 );

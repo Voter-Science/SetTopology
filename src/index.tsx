4203e5a7f844ce2a4e88b98e58a62b70cbae20b2
Voter-Science <Info@Voter-Science.com>
2019-04-27 16:55:36 -0700

    Initial load

--- src/index.tsx
+++ src/index.tsx
@@ -3,42 +3,69 @@ import * as ReactDOM from "react-dom";
 
 import { Hello } from "./components/Hello";
 
+import * as XC from 'trc-httpshim/xclient'
+import * as common from 'trc-httpshim/common'
+import * as core from 'trc-core/core'
+import * as trcSheet from 'trc-sheet/sheet'
+
 // declare var _sheetRef : any; // set by PluginMain, after page load.
 
 export interface IMajorProps { }
 export interface IMajorState {
-    AuthToken: string;
+    //AuthToken: string;
+    SheetClient: trcSheet.SheetClient;
     SheetId: string;
+
+    _info: trcSheet.ISheetInfoResult;
+    // Sheet Contents?  Sheet History? 
+
 }
 
 export class Major extends React.Component<{}, IMajorState> {
+
     public constructor(props: any) {
         super(props);
-        // this.state = { SheetId: undefined };
 
         var x: any = window;
         x.mainMajor = this;
     }
     render() {
-        return <h1>Major! {this.state.SheetId}</h1>;
+        if (!this.state) {
+            return <div>Loading...</div>
+        } else {
+            if (!this.state._info) {
+                return <div>Major! Not yet loaded: {this.state.SheetId}</div>;
+            } else {
+                return <div>Major: {this.state._info.Name}</div>
+            }
+        }
     }
 
     // Timer to pick up _sheetRef? 
 
     public setSheetRef(sheetRef: any): void {
+        var httpClient = XC.XClient.New(sheetRef.Server, sheetRef.AuthToken, undefined);
+        var sheetClient = new trcSheet.SheetClient(httpClient, sheetRef.SheetId);
+
         // Make async network call...
-        this.setState({ 
+        this.setState({
             SheetId: sheetRef.SheetId,
-            AuthToken : sheetRef.AuthToken
+            SheetClient: sheetClient
+        }, () => {
+            // State is updated 
+
+            this.state.SheetClient.getInfoAsync().then((info) => {
+                this.setState({ _info: info });
+            });
         });
     }
 }
 
 
 ReactDOM.render(
-    <p>
+    <div>
         <Major></Major>
         <Hello compiler="TypeScript" framework="React" />
-    </p>,
+    </div>,
     document.getElementById("example")
 );
